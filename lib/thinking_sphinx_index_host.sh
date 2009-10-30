27cc064ff0e316c9c71ea319a98c608fcf1fdbd5
time <time@godfingers.nodelivery>
2009-10-29 17:42:07 -0700

    add output for logfile

--- lib/thinking_sphinx_index_host.sh
+++ lib/thinking_sphinx_index_host.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo " ===== New Run at `date` ================================ "
+
 # simply reindex sphinx - the config will have all the deets
 cd /home/cassio/rails/inscryb
 rake thinking_sphinx:index
