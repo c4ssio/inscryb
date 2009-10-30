115f0c9d6bc68af933756829a6f8fdd8652c7a67
time <time@godfingers.nodelivery>
2009-10-29 18:39:07 -0700

    debugging done.

--- lib/thinking_sphinx_index_host.sh
+++ lib/thinking_sphinx_index_host.sh
@@ -2,11 +2,7 @@
 
 echo " ===== New Run at `date` ================================ "
 
-set -x
-
 # simply reindex sphinx - the config will have all the deets
 cd /home/cassio/rails/inscryb
 /usr/local/bin/rake thinking_sphinx:index
-
-set +x
 
