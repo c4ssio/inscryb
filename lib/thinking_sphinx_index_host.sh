002bbb89f931b841126d30a944dba84356042381
time <time@godfingers.nodelivery>
2009-10-29 17:44:40 -0700

    trying to debug why it's horked

--- lib/thinking_sphinx_index_host.sh
+++ lib/thinking_sphinx_index_host.sh
@@ -2,7 +2,11 @@
 
 echo " ===== New Run at `date` ================================ "
 
+set -x
+
 # simply reindex sphinx - the config will have all the deets
 cd /home/cassio/rails/inscryb
-rake thinking_sphinx:index
+/usr/local/bin/rake thinking_sphinx:index
+
+set +x
 
