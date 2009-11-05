f3551f119a42522224ff54d15bc04056cd81a52a
unknown <cpaesleme@.tfcorp.tfn.com>
2009-11-04 17:11:35 -0800

    fixed issue with allowing nils for username / password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@ class UsersController < ApplicationController
   def login
     case request.method
       when :post
-      if session[:user] = User.authenticate(params[:user][:name], params[:user][:password_hash])
+      if params[:user][:name].length>0 &&
+          params[:user][:password_hash].length>0 &&
+          session[:user] = User.authenticate(params[:user][:name], params[:user][:password_hash])
         flash[:notice]  = 'Login successful'
       else
         session[:user] = 1.u
