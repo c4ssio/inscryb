ed09fcd6e9bb51cc8a505736cebc96c479982c78
unknown <cpaesleme@.tfcorp.tfn.com>
2009-11-04 16:09:36 -0800

    added creator_id to oldtag reversion

--- app/models/old_tag.rb
+++ app/models/old_tag.rb
@@ -8,12 +8,12 @@ class OldTag < ActiveRecord::Base
     }
   end
 
-  def revert
+  def revert(args={})
     #re-attaches the tag
     if self.key=='parent_id'
-      self.thing_id.th.at(:in=>self.number.to_i)
+      self.thing_id.th.at(:in=>self.number.to_i,:creator_id=>(args[:creator_id] || 1))
     else
-      self.thing_id.th.at(self.key.to_sym => self.value)
+      self.thing_id.th.at(self.key.to_sym => self.value,:creator_id=>(args[:creator_id] || 1))
     end
   end
 
